f75365cbc9fcc8b8bbddbe2494886b4933810f0f
kaur1081 <karishmawalia@gmail.com>
2019-12-14 12:06:03 -0800

    updated bamazon Manager.js

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 !(https://kaur1081.github.io/bamazon/)
 
 ## Overview
-In this project we made node applications bamazonCustomers.js and bamazonManager.js.
+In this project we made Node applications bamazonCustomers.js and bamazonManager.js.
+Using inquirer,  MySql
 
 This project is useful as it helps to look for inventory at bamazon with  Include the ids, names, and prices of products for sale.
 
@@ -10,8 +11,7 @@ We can select and quit.
 It will update inventory show whats available.
 This project is useful in a store where you can buy, track and add inventories.
 Users can get started with the project in the terminal 
-
-This project is maintained  and contributed as an assignment in ucsd bootcamp by Karishma Kaur Hattar
+Project is maintained  and contributed by Karishma Kaur Hattar
 
 ## Screenshots
 ![ Bamazon ! ](https://github.com/kaur1081/bamazon/blob/master/bamazon.PNG)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,8 +5,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Harkirat02",
-  database: "bamazon"
+  password:"Harkirat02",
+  database:"bamazon"
 });
 
 connection.connect(function (err) {
@@ -16,62 +16,62 @@ connection.connect(function (err) {
 });
 
 var afterConnection = function () {
-connection.query("SELECT * FROM products", function (err, res) {
-for(var i=0; i<res.length; i++){
-  console.log(res[i].item_id+" || "
-  +res[i].product_name+"||" 
-  +res[i].department_name+"||"
-  +res[i].price+"||"
-  +res[i].stock_quantity+"\n");
-}
-userSelection(res);
+  connection.query("SELECT * FROM products", function (err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " || " +
+        res[i].product_name + "||" +
+        res[i].department_name + "||" +
+        res[i].price + "||" +
+        res[i].stock_quantity + "\n");
+    }
+    userSelection(res);
   })
 }
 
-var userSelection = function(res){
+var userSelection = function (res) {
   inquirer.prompt([{
-    type:'input',
+    type: 'input',
     name: 'choice',
-    message:"What would you like to buy?[quit with Q]"
+    message: "What would you like to buy?[quit with Q]"
 
-  }]).then(function(answer){
+  }]).then(function (answer) {
     var correct = false;
-    if (answer.choice.toUpperCase()=="Q"){
+    if (answer.choice.toUpperCase() == "Q") {
       process.exit();
     }
-    for(var i=0; i<res.length;i++){
-      if(res[i].product_name === answer.choice){
-        correct= true;
-        var product= answer.choice;
-        var id=i;
+    for (var i = 0; i < res.length; i++) {
+      if (res[i].product_name === answer.choice) {
+        correct = true;
+        var product = answer.choice;
+        var id = i;
         inquirer.prompt({
-          type:'input',
+          type: 'input',
           name: 'quantity',
-    message:"how much  would you like to buy?",
-    validate: function(value){
-      if(isNaN(value)===false){
-        return true;
-      }else{
-        return false;
+          message: "how much  would you like to buy?",
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true;
+            } else {
+              return false;
 
+            }
+          }
+        }).then(function (answer) {
+          if ((res[id].stock_quantity - answer.quantity) > 0) {
+            connection.query("UPDATE products SET stock_quantity='" + (res[id].stock_quantity - answer.quantity) + "'WHERE product_name='" + product + "'", function (err, res2) {
+              console.log("you got it!");
+              afterConnection();
+            })
+          } else {
+            console.log("opps we dont have it");
+            userSelection(res);
+          }
+        })
       }
-    } 
-         }).then(function(answer){
-           if((res[id].stock_quantity-answer.quantity)>0){
-             connection.query("UPDATE products SET stock_quantity='"+(res[id].stock_quantity-answer.quantity)+"'WHERE product_name='"+ product+"'",function(err,res2){
-               console.log("you got it!");
-               afterConnection();
-             })
-           }else{
-             console.log("opps we dont have it");
-             userSelection(res);
-           }
-          })
     }
-  }
-  if(i == res.length && correct == false){
-    console.log("not a valid choice");
-    userSelection(res);
-  }
-})
+    if (i == res.length && correct == false) {
+      console.log("not a valid choice");
+      userSelection(res);
+    }
+  })
 }
